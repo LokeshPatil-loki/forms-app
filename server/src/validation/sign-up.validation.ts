46f6fb27e4b2ee305260a2bdca7eb75bd6bd3477
LokeshPatil-loki <lokesh1404@outlook.com>
2024-12-11 02:43:18 +0530

    feat(server): create signup validation schema

---
+++ server/src/validation/sign-up.validation.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+export const SignUpValidationSchema = z.object({
+  body: z.object({
+    firstName: z
+      .string({ required_error: "firstName is required" })
+      .min(2)
+      .max(255),
+    lastName: z
+      .string({ required_error: "Last Name is required" })
+      .min(2)
+      .max(255),
+    email: z
+      .string({ required_error: "Email is required" })
+      .email("Invalid email format"),
+    password: z
+      .string({ required_error: "Password is required" })
+      .min(6, "Password must be at least 6 characters long")
+      .regex(/[a-zA-Z]/, {
+        message: "Password must include at least one letter",
+      })
+      .regex(/[0-9]/, { message: "Password must include at least one number" })
+      .regex(/[!@#$%^&*(),.?":{}|<>]/, {
+        message: "Password must include at least one special character",
+      }),
+  }),
+});
