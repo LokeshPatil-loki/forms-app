e90658307f537ffcb7a3656f36bcb340fcea59ba
LokeshPatil-loki <lokesh1404@outlook.com>
2024-12-11 01:18:16 +0530

    feat(server): add server export and handle app lifecycle events

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,3 +15,5 @@ app.use(errorHandler);
 // app.listen(port, () => {
 //   console.log(`Server is running at http://localhost:${port}`);
 // });
+
+export { app };
--- server/src/index.ts
+++ server/src/index.ts
@@ -23,3 +23,16 @@ try {
   console.error("MONGODB connection failed !!! ", error);
   process.exit(1);
 }
+
+import { app } from "./app";
+
+const PORT = process.env.PORT || 4000;
+
+app.on("error", (error) => {
+  console.log("ERROR ", error);
+  process.exit(1);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port: ${PORT}`);
+});
