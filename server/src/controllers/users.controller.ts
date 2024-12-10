a0d4a9f80824f02f48f04eda921b80fb09b31f52
LokeshPatil-loki <lokesh1404@outlook.com>
2024-12-11 03:24:54 +0530

    feat(server): hash password in sign up

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -2,15 +2,17 @@ import { Request, Response } from "express";
 import { asyncHanlder } from "../utils/async-handler";
 import { UserModel } from "../models/user.model";
 import { CustomError } from "../utils/CustomError";
+import { PasswordUtils } from "../utils/PasswordUtils";
 
 export const signUpController = asyncHanlder(
   async (req: Request, res: Response) => {
     const { firstName, lastName, email, password } = req.body;
+    const hashedPassword = await PasswordUtils.hashPassword(password);
     const user = await UserModel.create({
       firstName,
       lastName,
       email,
-      password,
+      password: hashedPassword,
     });
     if (!user) {
       throw new CustomError("Cannot create user", 500);
@@ -20,4 +22,8 @@ export const signUpController = asyncHanlder(
   }
 );
 
-const loginController = asyncHanlder(async (req: Request, res: Response) => {});
+export const loginController = asyncHanlder(
+  async (req: Request, res: Response) => {
+    return res.status(200).json({ user: req.body });
+  }
+);
