116c9e4778fbfe8676b999518f012cbcaf8c1d35
LokeshPatil-loki <lokesh1404@outlook.com>
2024-12-11 04:27:30 +0530

    refractor(server)

--- server/src/utils/JwtService.ts
+++ server/src/utils/JwtService.ts
@@ -3,19 +3,16 @@ import { UserModel } from "../models/user.model";
 import jwt from "jsonwebtoken";
 import { appConfig } from "../config/app.config";
 import { UnauthorizedError } from "../errors/UnauthorizedError";
+import { UserPayload } from "../types/user-payload";
+
 export class JwtService {
-  static generateToken(userData: {
-    _id: Types.ObjectId;
-    email: string;
-    firstName: string;
-    lastName: string;
-  }) {
+  static generateToken(userData: UserPayload) {
     return jwt.sign(userData, appConfig.JWT_SECRET!);
   }
 
   static verifyToken(token: string) {
     try {
-      return jwt.verify(token, appConfig.JWT_SECRET!);
+      return jwt.verify(token, appConfig.JWT_SECRET!) as UserPayload;
     } catch (error) {
       throw new UnauthorizedError("Invalid JWT Token");
     }
