4ff9df6931099a8790a8fd41db95db73fb6e272b
LokeshPatil-loki <lokesh1404@outlook.com>
2024-12-11 03:09:06 +0530

    feat(server): completed signup

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -1,6 +1,23 @@
 import { Request, Response } from "express";
 import { asyncHanlder } from "../utils/async-handler";
+import { UserModel } from "../models/user.model";
+import { CustomError } from "../utils/CustomError";
 
-export const signUpController = asyncHanlder((req: Request, res: Response) => {
-  return res.json(req.body);
-});
+export const signUpController = asyncHanlder(
+  async (req: Request, res: Response) => {
+    const { firstName, lastName, email, password } = req.body;
+    const user = await UserModel.create({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+    if (!user) {
+      throw new CustomError("Cannot create user", 500);
+    }
+    const data = await UserModel.findById(user._id, { password: 0 });
+    return res.status(201).json({ user: data });
+  }
+);
+
+const loginController = asyncHanlder(async (req: Request, res: Response) => {});
