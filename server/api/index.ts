8dab6b2a968a19c5b688ca387a98eb6258ac5373
LokeshPatil-loki <lokesh1404@outlook.com>
2024-12-12 20:02:00 +0530

    feat(server): connect to db from vercel serverless

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,2 +1,17 @@
+import { environmentVariableValidation } from "../src/config/app.config";
+import { connectDB } from "../src/db";
 import { app } from "../src/app";
+
+const start = async () => {
+  environmentVariableValidation();
+  try {
+    await connectDB();
+  } catch (error) {
+    console.error("MONGODB connection failed !!! ", error);
+    process.exit(1);
+  }
+};
+
+start();
+
 export default app;
