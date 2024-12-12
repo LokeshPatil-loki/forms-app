8c6510176cad261a0bf5d7e704b0bf566d039145
LokeshPatil-loki <lokesh1404@outlook.com>
2024-12-12 13:20:53 +0530

    (server):  modify response validation

--- server/src/validation/response/base-response.validation.ts
+++ server/src/validation/response/base-response.validation.ts
@@ -14,13 +14,16 @@ const ReponseItemSchema = z.object({
     .refine((value) => value !== undefined, { message: "Answer is required" }),
 });
 
+const ResponseItemsSchema = z.array(ReponseItemSchema);
+export type ResponseItemsType = z.infer<typeof ResponseItemsSchema>;
+export type ResponseItemType = z.infer<typeof ReponseItemSchema>;
 export const BaseResponseSchema = z.object({
   form: z
     .string({ required_error: "Form ID is requried" })
     .length(24, "Invalid Form ID"),
-  respondent: z
-    .string({ required_error: "Respondent ID is requried" })
-    .length(24, "Invalid Respondent ID"),
+  // respondent: z
+  //   .string({ required_error: "Respondent ID is requried" })
+  //   .length(24, "Invalid Respondent ID"),
   responses: z.array(ReponseItemSchema),
   submittedAt: z.date().default(new Date()),
 });
--- server/src/validation/response/create-response.validation.ts
+++ server/src/validation/response/create-response.validation.ts
@@ -1,11 +1,6 @@
 import { z } from "zod";
 import { BaseResponseSchema } from "./base-response.validation";
 
-export const CreateResponseValidation = z.object({
+export const CreateResponseSchema = z.object({
   body: BaseResponseSchema.strict(),
-  params: z.object({
-    formId: z
-      .string({ required_error: "formId is required" })
-      .length(24, "Invalid formId"),
-  }),
 });
