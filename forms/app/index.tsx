5627403c78e7035b07ac03f2c16fcbd18baa8a5e
LokeshPatil-loki <lokesh1404@outlook.com>
2024-12-14 00:09:57 +0530

    refactor(HomePage): remove unused Button component and authentication logic; simplify layout structure

--- forms/app/index.tsx
+++ forms/app/index.tsx
@@ -1,19 +1,11 @@
-import { Button } from "@/components/common/Button/Button";
-import { useAuthStore } from "@/stores/auth-store";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 const HomePage = () => {
-  // const { clearAuth } = useAuthStore();
-  // clearAuth();
   return (
     <View
       style={{ display: "flex", height: "100%" }}
-      className="items-center justify-center bg-fill"
-    >
-      <Button variant="primary" size="md" className="active:bg-button-accent ">
-        Click Me
-      </Button>
-    </View>
+      className="justify-center bg-fill"
+    ></View>
   );
 };
 
