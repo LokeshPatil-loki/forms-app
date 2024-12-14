d0e7add443d50b6e97034422486a705aae8ece14
LokeshPatil-loki <lokesh1404@outlook.com>
2024-12-14 23:49:42 +0530

    refactor(auth): update useSignUp and useLogin hooks to utilize generic ApiResponse type for improved type safety and consistency in API response handling

--- forms/hooks/use-auth.ts
+++ forms/hooks/use-auth.ts
@@ -1,15 +1,17 @@
 import { authApi } from "@/lib/api/endpoints/auth";
 import { useAuthStore } from "@/stores/auth-store";
-import { AuthResponse, LoginData, SignUpData } from "@/types/auth";
+import { LoginData, SignUpData } from "@/types/auth";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { router } from "expo-router";
 import { AxiosError } from "axios";
 import { ApiError } from "@/types/auth";
 import { notify } from "react-native-notificated";
+import { AuthResponse } from "@/types/api/auth-response";
+import { ApiResponse } from "@/types/api/api-response";
 
 export function useSignUp() {
   const setAuth = useAuthStore((state) => state.setAuth);
-  return useMutation<AuthResponse, ApiError, SignUpData>({
+  return useMutation<ApiResponse<AuthResponse>, ApiError, SignUpData>({
     mutationFn: async (data: SignUpData) => {
       try {
         const response = await authApi.signUp(data);
@@ -32,7 +34,7 @@ export function useSignUp() {
         throw "An unexpected error occurred";
       }
     },
-    onSuccess: (response: AuthResponse) => {
+    onSuccess: (response: ApiResponse<AuthResponse>) => {
       setAuth(response.data.user, response.data.token);
       router.replace("/(auth)/sign-in");
     },
@@ -41,7 +43,7 @@ export function useSignUp() {
 
 export function useLogin() {
   const setAuth = useAuthStore((state) => state.setAuth);
-  return useMutation<AuthResponse, string, LoginData>({
+  return useMutation<ApiResponse<AuthResponse>, string, LoginData>({
     mutationFn: async (data: LoginData) => {
       try {
         const response = await authApi.login(data);
