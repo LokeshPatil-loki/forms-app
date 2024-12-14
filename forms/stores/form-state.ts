2e4968b93fb14c7aaec45475afb8824afa65fe36
LokeshPatil-loki <lokesh1404@outlook.com>
2024-12-14 23:49:52 +0530

    feat(forms): implement Zustand store for form state management, including methods to set current form, add, update, and remove questions, and reset form state

--- forms/stores/form-state.ts
+++ forms/stores/form-state.ts
@@ -1,5 +1,6 @@
 import { Form } from "@/types/form";
 import { Question } from "@/types/question";
+import { create, createStore } from "zustand";
 
 interface FormState {
   currentForm: Form | null;
@@ -10,3 +11,61 @@ interface FormState {
   updateFormDetails: (details: Partial<Form>) => void;
   reset: () => void;
 }
+
+export const useFormStore = create<FormState>()((set) => ({
+  currentForm: null,
+  setCurrentForm: (form: Form) => set({ currentForm: form }),
+  addQuestion: (question: Question) => {
+    set((state) => {
+      if (!state.currentForm) return { currentForm: state.currentForm };
+
+      return {
+        currentForm: {
+          ...state.currentForm,
+          questions: [...(state.currentForm.questions || []), question],
+        },
+      };
+    });
+  },
+  updateQuestion: (index: number, question: Question) => {
+    set((state) => {
+      if (!state.currentForm) return { currentForm: state.currentForm };
+      const updatedQuestion = [...(state.currentForm.questions || [])];
+      if (index > 0 && index < updatedQuestion.length) {
+        updatedQuestion[index] = question;
+      }
+      return {
+        currentForm: {
+          ...state.currentForm,
+          questions: updatedQuestion,
+        },
+      };
+    });
+  },
+  removeQuestion: (index: number) => {
+    set((state) => {
+      if (!state.currentForm) return { currentForm: state.currentForm };
+
+      const updatedQuestions = [...(state.currentForm.questions || [])];
+      if (index > 0 && index < updatedQuestions.length) {
+        updatedQuestions.splice(index, 1);
+      }
+      return {
+        currentForm: {
+          ...state.currentForm,
+          questions: updatedQuestions,
+        },
+      };
+    });
+  },
+  updateFormDetails: (details: Partial<Form>) => {
+    set((state) => {
+      if (!state.currentForm) return { currentForm: state.currentForm };
+
+      return {
+        currentForm: { ...state.currentForm, ...details },
+      };
+    });
+  },
+  reset: () => set({ currentForm: null }),
+}));
