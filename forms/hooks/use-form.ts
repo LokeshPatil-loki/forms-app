018af859186dd78ec83cdeb342702c2cff988ff1
LokeshPatil-loki <lokesh1404@outlook.com>
2024-12-15 13:46:55 +0530

    feat(forms): enhance form management with new hooks for fetching, updating, and deleting forms; introduce useUpdateForm and useDeleteForm for improved API interactions and state management

--- forms/hooks/use-form.ts
+++ forms/hooks/use-form.ts
@@ -7,9 +7,23 @@ import {
   SingleFormResponse,
 } from "@/types/api/form-response";
 import { ValidationErrorResponse } from "@/types/error/validation-error-response";
-import { CreateFormData } from "@/types/form";
+import { CreateFormData, UpdateFormData } from "@/types/form";
 import { handleApiError } from "@/utils/handle-api-error";
 import { useMutation, useQuery } from "@tanstack/react-query";
+
+export function useGetMyForms() {
+  return useQuery<ApiResponse<FormListResponse>>({
+    queryFn: async () => handleApiError(() => formApi.getMyForms()),
+    queryKey: ["forms"],
+  });
+}
+
+export function useGetForm(formId: string) {
+  return useQuery<ApiResponse<SingleFormResponse>>({
+    queryFn: async () => handleApiError(() => formApi.getForm(formId)),
+    queryKey: ["forms", formId],
+  });
+}
 
 export function useCreateForm() {
   const { setCurrentForm } = useFormStore();
@@ -29,9 +43,36 @@ export function useCreateForm() {
   });
 }
 
-export function useGetMyForms() {
-  return useQuery<ApiResponse<FormListResponse>>({
-    queryFn: async () => handleApiError(() => formApi.getMyForms()),
-    queryKey: ["forms"],
+export function useUpdateForm() {
+  const { updateFormDetails } = useFormStore();
+  return useMutation<
+    ApiResponse<SingleFormResponse>,
+    ValidationErrorResponse,
+    { formId: string; data: UpdateFormData }
+  >({
+    mutationFn: async ({ formId, data }) =>
+      handleApiError(() => formApi.updateForm(formId, data)),
+    onSuccess: (response: ApiResponse<SingleFormResponse>) => {
+      queryClient.invalidateQueries({
+        queryKey: ["forms"],
+      });
+      updateFormDetails(response.data.form);
+    },
+  });
+}
+
+export function useDeleteForm() {
+  return useMutation<
+    ApiResponse<SingleFormResponse>,
+    ValidationErrorResponse,
+    string
+  >({
+    mutationFn: async (formId: string) =>
+      handleApiError(() => formApi.deleteForm(formId)),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["forms"],
+      });
+    },
   });
 }
