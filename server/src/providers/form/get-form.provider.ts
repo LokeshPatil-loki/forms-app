596dadace71af7f9d76c368b365438d838668280
LokeshPatil-loki <lokesh1404@outlook.com>
2024-12-18 14:14:04 +0530

    feat(forms): improve question population in getFormProvider by transforming question objects to include 'id' and remove '_id'

--- server/src/providers/form/get-form.provider.ts
+++ server/src/providers/form/get-form.provider.ts
@@ -8,7 +8,15 @@ export const getFormProvider = async (
   formId: string,
   loggedInUser: UserPayload
 ) => {
-  const form = await FormModel.findById(formId).populate("questions");
+  const form = await FormModel.findById(formId).populate({
+    path: "questions",
+    transform: (doc) => {
+      const questionObject = doc.toObject();
+      questionObject.id = questionObject._id;
+      delete questionObject._id;
+      return questionObject;
+    },
+  });
   if (!form) {
     throw new NotFoundError(ErrorMessages.FORM_NOT_FOUND);
   }
